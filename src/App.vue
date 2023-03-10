92f39c0c14033927433dff7840f43424d6f7d7d5
kazawan <55086403@qq.com>
2023-03-10 15:02:16 +0800

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/components/HelloWorld.vue 	new file:   src/components/TheWelcome.vue 	new file:   src/components/WelcomeItem.vue 	new file:   src/components/icons/IconCommunity.vue 	new file:   src/components/icons/IconDocumentation.vue 	new file:   src/components/icons/IconEcosystem.vue 	new file:   src/components/icons/IconSupport.vue 	new file:   src/components/icons/IconTooling.vue 	new file:   src/components/kaza-layout/kCol.vue 	new file:   src/components/kaza-layout/kRow.vue 	new file:   src/main.js 	new file:   vite.config.js

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <superbody win-size="24px" iphone-size="32px" >
+    <krow gutter="10" mb="10" mt="10"  >
+    <kcol span="8" xs="0" animation animation-delay=".1">
+      <div class="header">
+        #1-HEADER
+      </div>
+      
+    </kcol>
+    <kcol span="8" xs="24" animation animation-delay=".2" >
+      <div class="header">
+        #2-HEADER
+      </div>
+      
+    </kcol>
+    <kcol span="8" xs="24" animation animation-delay=".3" >
+      <div class="header">
+        #3-HEADER
+      </div>
+      
+    </kcol>
+  </krow>
+
+  <krow gutter="10" mb="10">
+    <kcol span="6" xs="0" md="24" animation animation-delay=".4">
+      <div class="side">SIDE</div>
+    </kcol>
+    <kcol span="18" xs="24" md="0" animation animation-delay=".5">
+      <div class="main">
+        MAIN
+      </div>
+    </kcol>
+  </krow>
+
+  <krow gutter="10" mb="10">
+    <kcol span="24" animation animation-delay=".1" >
+      <div class="footer">
+        FOOTER
+      </div>
+    </kcol>
+  </krow>
+  </superbody>
+  
+</template>
+
+
+<script setup>
+import krow from './components/kaza-layout/kRow.vue'
+import kcol from './components/kaza-layout/kCol.vue'
+import superbody from './components/kaza-layout/kSuperbody.vue'
+</script>
+
+<style lang="less">
+.root {
+  box-sizing: border-box;
+}
+
+.contianer {
+  width: 50%;
+  display: inline-block;
+}
+
+.header {
+  width: 100%;
+  
+  background-color:#EAB543 ;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  font-size: 1rem;
+}
+
+.side{
+  width: 100%;
+  height:800px ;
+  background-color:#55E6C1 ;
+  border-radius: 10px;
+}
+
+.main{
+  .side;
+  background-color: #FC427B;
+  font-size: 1rem;
+  // paddingj: 0;
+}
+
+.footer{
+  .header;
+  background-color:  #2C3A47;
+  color: white;
+}
+</style>
